c1537c12c69258e8d05f2e76b1d1e98d757f4fcf
Rolf Johansson <rojo@nocrew.org>
2014-08-10 00:22:12 +0200

    Add loading state

---
+++ App_Plugins/MediaContentUsage/mediacontentusage.controller.js
@@ -0,0 +1,11 @@
+angular.module("umbraco").controller("CTH.MediaContentUsageController", function ($scope, editorState, mediaContentUsageResource, notificationsService) {
+
+    // Indicate that we are loading
+	$scope.loading = true;
+    
+    // Get data about Content usage from our resource
+    mediaContentUsageResource.getMediaContentUsage(editorState.current.id).then(function (response) {
+		$scope.media = response.data;
+		$scope.loading = false;
+	});
+});
